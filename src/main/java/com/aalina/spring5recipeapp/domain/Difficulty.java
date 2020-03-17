cbf335af5cd97c57d8fd6baf28ec0370761ac9ef
aalina888 <alinaarap@gmail.com>
2020-03-17 17:24:30 +0200

    Difficulty Enum Added

---
+++ src/main/java/com/aalina/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.aalina.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/aalina/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/aalina/spring5recipeapp/domain/Recipe.java
@@ -21,14 +21,15 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private difficulty Difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
