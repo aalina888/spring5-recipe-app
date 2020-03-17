92358c5a2652c93302f8df6dceba3a0daf11e0bd
aalina888 <alinaarap@gmail.com>
2020-03-17 18:35:04 +0200

    Find By Description Method Added

--- src/main/java/com/aalina/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/aalina/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.aalina.spring5recipeapp.repositories;
 import com.aalina.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
