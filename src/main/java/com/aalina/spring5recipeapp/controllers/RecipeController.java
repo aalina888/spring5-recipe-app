aef4c49879c8355341ad692b718677ab6377e537
aalina888 <alinaarap@gmail.com>
2020-03-26 19:10:10 +0200

    Form for Adding a New Recipe Created

--- src/main/java/com/aalina/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/aalina/spring5recipeapp/controllers/RecipeController.java
@@ -1,13 +1,12 @@
 package com.aalina.spring5recipeapp.controllers;
 
+import com.aalina.spring5recipeapp.commands.RecipeCommand;
 import com.aalina.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @Controller
 public class RecipeController {
     private final RecipeService recipeService;
@@ -18,8 +17,23 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
