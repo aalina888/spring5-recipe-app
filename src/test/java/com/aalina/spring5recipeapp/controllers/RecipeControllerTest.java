ab168277d8c1ab89e292e9ecc2289492ef507cb5
aalina888 <alinaarap@gmail.com>
2020-04-16 21:26:49 +0300

    400 Bad Request Error Page Added

--- src/test/java/com/aalina/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/aalina/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -50,14 +50,20 @@ public class RecipeControllerTest {
 
     @Test
     public void getRecipeNotFoundTest() throws Exception {
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void getRecipeNumberFormatTest() throws Exception {
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
